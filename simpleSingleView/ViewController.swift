04b6d0df8af076ee42ef3353190ffcc35f27c329
Jock Christie <jock.christie@gmail.com>
2018-02-09 21:36:56 -0800

    20180209 Added SegmentedControll

---
+++ simpleSingleView/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  simpleSingleView
+//
+//  Created by Suzanne Christie on 2/9/18.
+//  Copyright © 2018 MKKI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func segmentedControlWasChanged() {
+        if self.seg
+    
+    }
+
+
+}
+
